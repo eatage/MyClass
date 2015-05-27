5a75854a790a70010a9e55e36fe6ec49958a0148
eatage <xuxinbo@outlook.com>
2015-05-27 16:26:14 +0800

    http

---
+++ MyClass/HttpGet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    /// <summary>
+    /// Get方法
+    /// </summary>
+    class HttpGet
+    {
+        /// <summary>
+        /// Get方法请求url并接收返回消息
+        /// </summary>
+        /// <param name="strUrl">Url地址</param>
+        /// <returns></returns>
+        public string GetPageInfo(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string ret = string.Empty;
+            Stream s;
+            string StrDate = "";
+            string strValue = "";
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                s = response.GetResponseStream();
+                ////在这儿处理返回的文本
+                StreamReader Reader = new StreamReader(s, Encoding.UTF8);
+
+                while ((StrDate = Reader.ReadLine()) != null)
+                {
+                    strValue += StrDate + "\r\n";
+                }
+                //strValue = Reader.ReadToEnd();
+            }
+            return strValue;
+        }
+    }
+}
---
+++ MyClass/HttpPost.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    /// <summary>
+    /// Post方法
+    /// </summary>
+    class HttpPost
+    {
+        /// <summary>
+        /// Post方法
+        /// </summary>
+        /// <param name="Url">URL</param>
+        /// <param name="Params">Post数据 多个参数用&分隔</param>
+        /// <returns></returns>
+        public static string of_SendPost_utf8(string Url, string Params)
+        {
+            // 初始化WebClient  
+            System.Net.WebClient webClient = new System.Net.WebClient();
+            webClient.Headers.Add("Accept", "*/*");
+            webClient.Headers.Add("Accept-Language", "zh-cn");
+            webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            //将字符串转换成字节数组
+            byte[] postData = Encoding.GetEncoding("utf-8").GetBytes(Params);
+            try
+            {
+                byte[] responseData = webClient.UploadData(Url, "POST", postData);
+                string srcString = Encoding.GetEncoding("utf-8").GetString(responseData);
+                return srcString.Trim();
+            }
+            catch (Exception ex)
+            {
+                return "-1";
+            }
+        }
+
+        /// <summary>
+        /// Post方法
+        /// </summary>
+        /// <param name="posturl">URL</param>
+        /// <param name="postData">Post数据 多个参数用&分隔</param>
+        public string PostPage(string posturl, string postData)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = Encoding.UTF8;
+            byte[] data = encoding.GetBytes(postData);
+            // 准备请求...
+            try
+            {
+                // 设置参数
+                request = WebRequest.Create(posturl) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }
+        }
+    }
+}
